e0ed77ec8a9c9e178ca9d481ad486ec41ba1ac77
PercyDan <percydan@qq.com>
2020-07-12 22:20:12 +0800

    initial commit

---
+++ src/main/java/me/PercyDan/RandomTitle/Mixins/MixinMinecraft.java
@@ -0,0 +1,76 @@
+package me.PercyDan.RandomTitle.Mixins;
+
+import me.PercyDan.RandomTitle.RandomTitleUtil;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.SharedConstants;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.Window;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.integrated.IntegratedServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.text.SimpleDateFormat;
+
+
+@Mixin(MinecraftClient.class)
+public abstract class MixinMinecraft {
+	@Shadow
+	public abstract boolean isModded();
+	@Shadow
+	private IntegratedServer server;
+	@Shadow
+	public abstract ClientPlayNetworkHandler getNetworkHandler();
+	@Shadow
+	public abstract boolean isConnectedToRealms();
+	@Shadow
+	public Screen currentScreen;
+	@Shadow
+	public ClientWorld world;
+	@Shadow
+	private ClientConnection connection;
+	@Shadow
+	private Window window;
+	@Shadow
+	private ServerInfo currentServerEntry;
+	public String Randtitle= RandomTitleUtil.getTitle();
+	@Inject(method = "getWindowTitle", at = @At("HEAD"),cancellable = true)
+	private void inject_getWindowTitle(CallbackInfoReturnable<String> ci) {
+		final Logger LOGGER = LogManager.getLogger("RandomTitle");
+		String title = RandomTitleUtil.getFormat();
+		String prefix = RandomTitleUtil.getPrefix();
+		StringBuilder stringBuilder = new StringBuilder(SharedConstants.getGameVersion().getName());
+		stringBuilder.append(" ");
+		ClientPlayNetworkHandler clientPlayNetworkHandler = this.getNetworkHandler();
+		if (clientPlayNetworkHandler != null && clientPlayNetworkHandler.getConnection().isOpen()) {
+			stringBuilder.append(" - ");
+			if (this.server != null && !this.server.isRemote()) {
+				stringBuilder.append(I18n.translate("title.singleplayer", new Object[0]));
+			} else if (this.isConnectedToRealms()) {
+				stringBuilder.append(I18n.translate("title.multiplayer.realms", new Object[0]));
+			} else if (this.server == null && (this.currentServerEntry == null || !this.currentServerEntry.isLocal())) {
+				stringBuilder.append(I18n.translate("title.multiplayer.other", new Object[0]));
+			} else {
+				stringBuilder.append(I18n.translate("title.multiplayer.lan", new Object[0]));
+			}
+
+		}
+		String date = new SimpleDateFormat(RandomTitleUtil.getDateFormat()).format((System.currentTimeMillis()));
+		prefix = prefix.replace("%version%", stringBuilder.toString());
+		title=title.replace("%date%", date);
+		title=title.replace("%prefix%",prefix);
+		title=title.replace("%title%", Randtitle);
+		LOGGER.info("Title set to \"" + title + "\"");
+		ci.setReturnValue(title);
+		ci.cancel();
+	}
+}
---
+++ src/main/java/me/PercyDan/RandomTitle/RandomTitleUtil.java
@@ -0,0 +1,116 @@
+package me.PercyDan.RandomTitle;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+
+public class RandomTitleUtil {
+    private static final Logger LOGGER = LogManager.getLogger("RandomTitleUtil");
+    static Map yaml;
+    static File configFile=new File("title.yml");
+    static {
+        if(configFile.exists()){
+        try {
+            yaml = new Yaml().load(new FileInputStream("title.yml"));
+        } catch (Throwable e) {
+            LOGGER.catching(e);
+        }
+    }}
+
+
+    public static String getTitleFromList() {
+        String title = null;
+        try {
+            List titles = (List) yaml.get("title");
+            title = titles.get(new Random().nextInt(titles.size())).toString();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return title;
+    }
+
+    public static String getTitleFromHitokoto() {
+        LOGGER.info("Getting title from hitokoto");
+        String title;
+        String response = null;
+        try {
+            response = EntityUtils.toString(HttpClients.createDefault().execute(new HttpGet("https://v1.hitokoto.cn/")).getEntity());
+            LOGGER.info("Response String:\n" + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JsonObject jsonObject = new JsonParser().parse(response).getAsJsonObject();
+        String from = jsonObject.get("from").getAsString();
+        String sentence = jsonObject.get("hitokoto").getAsString();
+        if (from.contentEquals("网络")) {
+            title = sentence + "   ——来自" + from;
+        }
+        if (from.contains("《") || from.contains("》") || from.contentEquals("原创")) {
+            title = sentence + "   ——" + jsonObject.get("creator") + jsonObject.get("from").getAsString();
+        } else {
+            title = sentence + "   ——《" + jsonObject.get("from").getAsString() + "》";
+        }
+        return title;
+    }
+
+    public static String getTitle() {
+        if(!configFile.exists()){
+            LOGGER.error("Config file not found!");
+            return getTitleFromHitokoto();
+        }
+        int mode = (int) yaml.get("mode");
+        switch (mode) {
+            case 0:
+                return getTitleFromHitokoto();
+            case 1:
+                return getTitleFromList();
+            case 2:
+                boolean use = new Random().nextBoolean();
+                if (use) {
+                    return getTitleFromList();
+                } else {
+                    return getTitleFromHitokoto();
+                }
+        }
+        return getTitleFromList();
+    }
+
+    public static String getPrefix() {
+        if(configFile.exists()) {
+            List prefixes = (List) yaml.get("prefix");
+            String prefix = prefixes.get((new Random()).nextInt(prefixes.size())).toString();
+            return prefix;
+        }
+           return "Minecraft %version%";
+    }
+
+    public static String getFormat() {
+        if(configFile.exists()) {
+            return (String) yaml.get("format");
+        }
+            return "%prefix%  |  %title%";
+    }
+
+    public static String getDateFormat() {
+        if(configFile.exists()) {
+            return (String) yaml.get("dateformat");
+        }
+            return "yyyy年MM月dd日 hh:mm:ss";
+    }
+
+}
+
